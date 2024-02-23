82eeba149fcd7981964123a45a930f48d92765f3
Manuel-Alexander Oppermann <manuel.alexander.oppermann@gmail.com>
2024-02-23 02:00:01 +0100

    Add lsp plugin

--- .config/nvim/lua/iitsgiga/keymaps.lua
+++ .config/nvim/lua/iitsgiga/keymaps.lua
@@ -9,8 +9,27 @@ vim.keymap.set("v", "Y", "\"+y") -- yank into system clipboard
 -- move selection up/down
 vim.keymap.set("n", "<C-k>", ":m .-2<CR>==")
 vim.keymap.set("n", "<C-j>", ":m .+1<CR>==")
-vim.keymap.set("i", "<C-k>", "<ESC>:m .-2<CR>==gi")
-vim.keymap.set("i", "<C-j>", "<ESC>:m .+1<CR>==gi")
+-- vim.keymap.set("i", "<C-k>", "<ESC>:m .-2<CR>==gi")
+-- vim.keymap.set("i", "<C-j>", "<ESC>:m .+1<CR>==gi")
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
 
+-- lsp
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "<leader>vh", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vf", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        -- vim.keymap.set("i", "<C-n>", function() vim.lsp.buf.completion() end, opts)
+        vim.keymap.set("n", "<leader>dn", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "<leader>dN", function() vim.diagnostic.goto_prev() end, opts)
+    end,
+})
---
+++ .config/nvim/lua/iitsgiga/plugins/lsp.lua
@@ -0,0 +1,118 @@
+local language_servers = {
+    "lua_ls", "bashls", "dotls", "dockerls", "texlab", --"autotools-language-server",
+    "cssls", "html", "htmx", "jsonls", "marksman", "templ", "lemminx", "yamlls",
+    "cypher_ls", "graphql", "sqlls",
+    "tsserver", "intelephense",
+    "rust_analyzer", "gopls", "jdtls", "jedi_language_server",
+    "asm_lsp", "clangd", "fortls", "arduino_language_server", -- "r_language_server",
+}
+
+local server_settings = {
+    ["lua_ls"] = {
+        globals = { "vim" }
+    },
+
+    ["fortls"] = {
+        cmd = {
+            "fortls",
+            "--lowercase_intrinsics",
+            "--hover_signature",
+            "--hover_language=fortran",
+            "--use_signature_help",
+            "--max_line_length=132",
+            "--max_comment_line_length=132",
+        }
+    },
+
+    ["clangd"] = {
+        cmd = {
+            "clangd",
+            "--clang-tidy",
+        }
+    },
+}
+
+return { -- https://www.youtube.com/watch?v=MuUrCcvE-Yw
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+    },
+
+    config = function()
+        -- setup fidget.nvim
+        require('fidget').setup {}
+
+        -- setup nvim-cmp
+        local cmp = require('cmp')
+        cmp.setup {
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<Tab>'] = cmp.mapping.select_next_item(),
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                ['<C-j>'] = cmp.mapping.scroll_docs(4),
+                ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                { name = 'buffer' },
+            })
+        }
+
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            require('cmp_nvim_lsp').default_capabilities()
+        )
+
+        -- setup language servers
+        require("mason").setup()
+        require("mason-lspconfig").setup {
+            ensure_installed = language_servers,
+            handlers = {
+                function (server_name) --default handler
+                    local settings = vim.tbl_deep_extend(
+                        "force",
+                        {},
+                        server_settings[server_name] or {},
+                        {
+                            on_attach = on_attach,
+                            capabilities = capabilities,
+                        }
+                    )
+                    require("lspconfig")[server_name].setup(settings)
+                end,
+            },
+        }
+
+        -- setup diagnostic
+        vim.diagnostic.config({
+            update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+    end,
+}
